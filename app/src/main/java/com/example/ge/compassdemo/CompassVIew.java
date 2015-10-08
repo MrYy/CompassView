f257ea8a1a2ca3e3f27b108ffb7cbb2563cefdb5
MrYy <254117440@qq.com>
2015-10-08 16:38:32 +0800

    有了compassdemo基本图，下一步要结合传感器

--- app/src/main/java/com/example/ge/compassdemo/CompassVIew.java
+++ app/src/main/java/com/example/ge/compassdemo/CompassVIew.java
@@ -2,15 +2,26 @@ package com.example.ge.compassdemo;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 /**
  * Created by ge on 2015/10/8.
  */
 public class CompassVIew extends View {
     private float bearing;
+    private Paint markerPaint;
+    private Paint textPaint;
+    private Paint circlePaint;
+    private String northString;
+    private String eastString;
+    private String westString;
+    private String southString;
+    private int textHeight;
 
     public CompassVIew(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
@@ -29,19 +40,81 @@ public class CompassVIew extends View {
         super(context);
         initCompassView();
     }
-    private Paint markerPaint;
-    private Paint textPaint;
-    private Paint circlePaint;
-    private String northString;
-    private String eastString;
-    private String westString;
-    private String SouthString;
-    private int textHeight;
 
     public void initCompassView() {
         setFocusable(true);
-        Resources r=this.getResources();
-        
+        Resources r = this.getResources();
+
+        circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        circlePaint.setColor(r.getColor(R.color.background_color));
+        circlePaint.setStrokeWidth(1);
+        circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        northString = r.getString(R.string.cardinal_north);
+        eastString = r.getString(R.string.cardinal_east);
+        westString = r.getString(R.string.cardinal_west);
+        southString = r.getString(R.string.cardinal_south);
+
+        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        textPaint.setColor(r.getColor(R.color.text_color));
+
+
+        textHeight = (int) textPaint.measureText("yY");
+        markerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        markerPaint.setColor(r.getColor(R.color.marker_color));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int mMeasuredWidth = getMeasuredWidth();
+        int mMeasuredHeight = getMeasuredHeight();
+        int px = mMeasuredWidth / 2;
+        int py = mMeasuredHeight / 2;
+        int radius = Math.min(px, py);
+        canvas.drawCircle(px, py, radius, circlePaint);
+
+        canvas.save();
+        canvas.rotate(-bearing, px, py);
+        int textWidth = (int) textPaint.measureText("W");
+        int cardinalX = px - textWidth / 2;
+        int cardinalY = py - radius + textHeight;
+        for (int i = 0; i < 24; i++) {
+            canvas.drawLine(px, py - radius, px, py - radius + 10, markerPaint);
+            canvas.save();
+            canvas.translate(0, textHeight);
+            if (i % 6 == 0) {
+                String dirString = "";
+                switch (i) {
+                    case 0:
+                        dirString = northString;
+                        int arrowY = 2 * textHeight;
+                        canvas.drawLine(px, arrowY, px - 5, 3 * textHeight, markerPaint);
+                        canvas.drawLine(px, arrowY, px + 5, 3 * textHeight, markerPaint);
+                        break;
+                    case 6:
+                        dirString = eastString;
+                        break;
+                    case 12:
+                        dirString = southString;
+                        break;
+                    case 18:
+                        dirString = westString;
+                        break;
+                }
+                canvas.drawText(dirString, cardinalX, cardinalY, textPaint);
+            } else if (i % 3 == 0) {
+                String angle=String.valueOf(i*15);
+                float angleTextWidth=textPaint.measureText(angle);
+
+                int angleTextX=(int)(px-angleTextWidth/2);
+                int angleTextY=(int)(py-radius+textHeight);
+                canvas.drawText(angle,angleTextX,angleTextY,textPaint);
+            }
+            canvas.restore();
+            canvas.rotate(15,px,py);
+        }
+        canvas.restore();
+        Log.d("TEST","FINISH DRAWING");
     }
 
     @Override
@@ -65,14 +138,31 @@ public class CompassVIew extends View {
             result = specSize;
         }
         return result;
-    }
 
-    public void setBearing(float bearing) {
-        this.bearing = bearing;
     }
 
     public float getBearing() {
         return bearing;
+
     }
 
+    public void setBearing(float bearing) {
+        this.bearing = bearing;
+        sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+    }
+
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(final AccessibilityEvent event) {
+        super.dispatchPopulateAccessibilityEvent(event);
+        if(isShown()){
+            String bearingStr=String.valueOf(bearing);
+            if(bearingStr.length()>AccessibilityEvent.MAX_TEXT_LENGTH){
+                bearingStr=bearingStr.substring(0,AccessibilityEvent.MAX_TEXT_LENGTH);
+            }
+            event.getText().add(bearingStr);
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
